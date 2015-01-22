ee1cffa291dc90e28d7a8fcdde82ef5242aae476
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2015-01-22 18:16:28 +0800

    init

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+  "fmt"
+  "github.com/codegangsta/cli"
+  "os"
+  "os/exec"
+)
+
+func main() {
+
+  app := cli.NewApp()
+  app.Name = "belt"
+  app.Usage =  "tool-kit for x509"
+  app.Commands = []cli.Command {
+    {
+      Name: "chkcrt",
+      Action: func (c *cli.Context) {
+
+        file := c.Args().First()
+        cmd := exec.Command("openssl", "x509", "-in", file, "-text", "-noout")
+        output, err := cmd.Output()
+        if err != nil {
+          panic(err)
+        }
+
+        fmt.Println(string(output))
+      },
+    },
+  }
+
+  app.Run(os.Args)
+}

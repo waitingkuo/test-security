8a656292deb728dc4e016750a065e2a4ba4420cc
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2015-02-22 01:39:54 +0800

    fix

--- main.go
+++ main.go
@@ -1,59 +1,60 @@
 package main
 
 import (
-  "fmt"
-  "github.com/codegangsta/cli"
-  "os"
-  "os/exec"
+	"fmt"
+	"github.com/codegangsta/cli"
+	"os"
+	"os/exec"
 )
 
 func main() {
 
-  app := cli.NewApp()
-  app.Name = "belt"
-  app.Usage =  "tool-kit for x509"
-  app.Commands = []cli.Command {
-    {
-      Name: "mkkey2048",
-      Action: func (c *cli.Context) {
-        cmd := exec.Command("openssl", "genrsa", "2048")
-        output, err := cmd.Output()
-        if err != nil {
-          panic(err)
-        }
+	app := cli.NewApp()
+	app.Name = "belt"
+	app.Usage = "tool-kit for x509"
+	app.Commands = []cli.Command{
+		{
+			Name: "mkkey",
+			Action: func(c *cli.Context) {
+				cmd := exec.Command("openssl", "genrsa", "2048")
+				output, err := cmd.Output()
+				if err != nil {
+					panic(err)
+				}
 
-        fmt.Println(string(output))
+				fmt.Println(string(output))
 
-      },
-    },
-    {
-      Name: "mkcrt",
-      Action: func (c *cli.Context) {
-        cmd := exec.Command("openssl", "req", "-new")
-        output, err := cmd.Output()
-        if err != nil {
-          panic(err)
-        }
+			},
+		},
+		{
+			Name: "mkcrt",
+			Action: func(c *cli.Context) {
+				cmd := exec.Command("openssl", "req", "-new")
+				output, err := cmd.Output()
+				if err != nil {
+					panic(err)
+				}
 
-        fmt.Println(string(output))
+				fmt.Println(string(output))
 
-      },
-    {
-      Name: "chkcrt",
-      Action: func (c *cli.Context) {
+			},
+		},
+		{
+			Name: "chkcrt",
+			Action: func(c *cli.Context) {
 
-        file := c.Args().First()
-        //cmd := exec.Command("openssl", "x509", "-in", file, "-text", "-noout")
-        cmd := exec.Command("openssl", "x509", "-in", file, "-text")
-        output, err := cmd.Output()
-        if err != nil {
-          panic(err)
-        }
+				file := c.Args().First()
+				//cmd := exec.Command("openssl", "x509", "-in", file, "-text", "-noout")
+				cmd := exec.Command("openssl", "x509", "-in", file, "-text")
+				output, err := cmd.Output()
+				if err != nil {
+					panic(err)
+				}
 
-        fmt.Println(string(output))
-      },
-    },
-  }
+				fmt.Println(string(output))
+			},
+		},
+	}
 
-  app.Run(os.Args)
+	app.Run(os.Args)
 }

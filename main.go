a3e409ffd2696071ff1cd6af94e5c5240952d24a
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2015-02-25 03:30:21 +0800

    fix

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	app.Usage = "tool-kit for x509"
 	app.Commands = []cli.Command{
 		{
-			Name: "mkkey",
+			Name: "mkkey2048",
 			Action: func(c *cli.Context) {
 				cmd := exec.Command("openssl", "genrsa", "2048")
 				output, err := cmd.Output()
